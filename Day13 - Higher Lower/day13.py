35aa0ddc95b7a77b8527e7a036db3c71c3c13ded
saranath <saranathgupta@gmail.com>
2024-10-23 22:29:25 +0400

    Day13 Project Refactor

--- Day13 - Higher Lower/day13.py
+++ Day13 - Higher Lower/day13.py
@@ -2,8 +2,6 @@ import art
 import random
 import game_data
 
-print(art.logo)
-
 
 def get_name(obj):
     return obj["name"]
@@ -21,24 +19,44 @@ def get_follower_count(obj):
     return obj["follower_count"]
 
 
+def compare_follower_count(guess, ffc, sfc):
+    if ffc > sfc:
+        return guess == "A"
+    else:
+        return guess == "B"
+
+
+def format_data(obj):
+    name = obj["name"]
+    description = obj["description"]
+    country = obj["country"]
+    return f"{name}, a {description}, from {country}"
+
+
 def higher_lower():
-    first_choice_data = random.choice(game_data.data)
-    second_choice_data = random.choice(game_data.data)
     # should_continue is a flag that determine, if game should be continued or stopped
     should_continue = True
+    print(art.logo)
     # number of times user guess the right option.
     hit_count = 0
-    print(
-        f"Compare A: {get_name(first_choice_data)}, a {get_description(first_choice_data)}, from {get_country(first_choice_data)}")
-    print(art.vs)
-    print(
-        f"Compare B: {get_name(second_choice_data)}, a {get_description(second_choice_data)}, from {get_country(second_choice_data)}")
-    first_choice_follower_count = get_follower_count(first_choice_data)
-    secnd_choice_follower_count = get_follower_count(second_choice_data)
+    first_choice_data = random.choice(game_data.data)
+    second_choice_data = random.choice(game_data.data)
     while should_continue:
         if hit_count > 0:
             print(f"You're right! Current score: {hit_count}")
+            first_choice_data = second_choice_data
+            second_choice_data = random.choice(game_data.data)
+
+        print(f"Compare A: {format_data(first_choice_data)}")
+        print(art.vs)
+        print(f"Against B: {format_data(second_choice_data)}")
+        first_choice_follower_count = get_follower_count(first_choice_data)
+        secnd_choice_follower_count = get_follower_count(second_choice_data)
+
         choice = input('''Who has more followers? Type either 'A' or 'B' to guess or press 'E' to exit the game: ''')
+
+        print("\n"*20)
+        print(art.logo)
 
         if choice == "E":
             should_continue = False
@@ -46,17 +64,15 @@ def higher_lower():
         elif choice != "A" and choice != "B":
             should_continue = False
             print(f"Sorry, that's wrong. Your score is : {hit_count}")
-        else:
-            if choice == "A" and first_choice_follower_count >= secnd_choice_follower_count:
+        elif choice == "A" or choice == "B":
+            should_continue = compare_follower_count(choice, first_choice_follower_count, secnd_choice_follower_count)
+            if should_continue:
                 hit_count += 1
-                second_choice_data = random.choice(game_data.data)
-            elif choice == "B" and secnd_choice_follower_count >= first_choice_follower_count:
-                hit_count += 1
-                first_choice_data = second_choice_data
-                second_choice_data = random.choice(game_data.data)
             else:
-                should_continue = False
                 print(f"Sorry, that's wrong. Your score is : {hit_count}")
+        else:
+            should_continue = False
+            print(f"Sorry, that's wrong. Your score is : {hit_count}")
 
 
 higher_lower()
